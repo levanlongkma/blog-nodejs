6b3e0adaaa5b862c9879a3b921867d024a28166d
longkma <levanlongkma@gmail.com>
2023-02-23 10:29:03 +0700

    [Update] start layout

--- src/index.js
+++ src/index.js
@@ -2,12 +2,23 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const morgan = require('morgan')
+const { engine } = require('express-handlebars');
 
-app.use(morgan('combined'))
+app.use(express.static('./src/public'));
+app.use(morgan('combined'));
+app.engine('hbs', engine({
+    extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', './src/resources/views');
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
-})
+    res.render('home');
+});
+
+app.get('/news', (req, res) => {
+    res.render('news');
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
